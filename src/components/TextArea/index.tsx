1e65a3e7430b23497cfde3de528d794d05da2ecb
Yazed Jamal <yazedjamal@Yazeds-MacBook-Air.local>
2025-11-03 20:25:00 +0000

    fix: control component

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { TextAreaProps } from "./types";
 import { StyledPixelBox } from "../SharedComponent/StyledPixelBox";
 import {
